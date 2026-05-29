3b4ad36f651c944f04effc304ffdbd620e72ac52
Hui Peng Hu <woohp135@gmail.com>
2026-05-29 01:23:31 -0700

    build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "./",
+    build: {
+        outDir: "docs",
+        emptyOutDir: true,
+    },
     plugins: [svelte(), tailwindcss()],
 });
